eae6250afc454bed096aca2edf605f9e39c00346
danielImdl <daniel.lmid@gmail.com>
2025-06-25 19:46:13 -0500

    basic visual of Form structure

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Form(){
+    const [file, setFile] = useState(null);
+
+    function handleFileChange(e){
+    }
+
+    function handleUpload(){
+
+    }
+
+
     return(
         <>
-            <div>Upload Form</div>
+            <div>
+                <input type="file" accept="image/*" onChange={handleFileChange} />
+                <button onClick = {handleUpload} disabled={!file}>
+                    Upload
+                </button>
+            </div>
         </>
     )
 }
