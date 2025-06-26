77f29e5c46c4f0c1fa2c3ac7e3b73b2000665316
danielImdl <daniel.lmid@gmail.com>
2025-06-25 19:29:12 -0500

    name convention fixed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+import ImageList from './components/imageList/ImageList';
+import UploadForm from './components/form/Form';
+import SearchBar from './components/searchBar/SearchBar';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Images Upload App</h1>
+      <UploadForm />
+      <SearchBar />
+      <ImageList />
     </div>
   );
 }
